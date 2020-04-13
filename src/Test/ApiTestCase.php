3855e771da32b5fd626b40fab2131da747903856
Tiago Felipe <tiago@tiagofelipe.com>
2020-04-13 08:39:54 -0300

    Dedicated test env file

--- src/Test/ApiTestCase.php
+++ src/Test/ApiTestCase.php
@@ -50,9 +50,9 @@ class ApiTestCase extends KernelTestCase
         $handler->push(Middleware::history(self::$history));
         $handler->push(Middleware::mapRequest(function (RequestInterface $request) {
             $path = $request->getUri()->getPath();
-            /*if (strpos($path, '/app_test.php') !== 0) {
+            if (strpos($path, '/app_test.php') !== 0) {
                 $path = '/app_test.php' . $path;
-            }*/
+            }
             $uri = $request->getUri()->withPath($path);
 
             return $request->withUri($uri);
