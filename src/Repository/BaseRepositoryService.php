ca95dedf489775a578a3c4e902f0dac93f5343b4
Tiago Felipe <tiago@tiagofelipe.com>
2020-04-25 18:54:24 -0300

    Created BaseRepositoryService

---
+++ src/Repository/BaseRepositoryService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Uloc\ApiBundle\Repository;
+
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepositoryInterface;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * Class BaseRepositoryService
+ * @package UlocApiBundle\Repository
+ */
+class BaseRepositoryService extends BaseRepository implements ServiceEntityRepositoryInterface
+{
+
+    /**
+     * @param string $entityClass The class name of the entity this repository manages
+     */
+    public function __construct(ManagerRegistry $registry, $entityClass)
+    {
+        $manager = $registry->getManagerForClass($entityClass);
+
+        if ($manager === null) {
+            throw new LogicException(sprintf(
+                'Could not find the entity manager for class "%s". Check your Doctrine configuration to make sure it is configured to load this entity’s metadata.',
+                $entityClass
+            ));
+        }
+
+        parent::__construct($manager, $manager->getClassMetadata($entityClass));
+    }
+
+
+}
