a9fedd5c07d873178f632c0567d0ecb5f014c7a0
Tiago Felipe <tiago@tiagofelipe.com>
2020-04-29 20:28:52 -0300

    Bugfix generatePassword UserManager

--- src/Manager/UserManager.php
+++ src/Manager/UserManager.php
@@ -40,7 +40,7 @@ class UserManager extends CustomManager implements UserManagerInterface
         $user->setUsername($username);
         $user->setEmail($email);
         if (empty($password)) {
-            $this->generatePassword();
+            $password = $this->generatePassword();
         }
         $user->setPlainPassword($password);
         if ($this->passwordEncoder) {
